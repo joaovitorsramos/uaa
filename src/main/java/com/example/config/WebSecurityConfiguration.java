c37ab4f0d8ea1d91412a19f8622079e52ce114fe
joaovitorsramos <joaovitor-soares@hotmail.com>
2020-07-07 11:36:49 -0300

    removing bd address and password

--- src/main/java/com/example/config/WebSecurityConfiguration.java
+++ src/main/java/com/example/config/WebSecurityConfiguration.java
@@ -5,14 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+
+	
     @Autowired
     private UserDetailsService userDetailsService;
 
